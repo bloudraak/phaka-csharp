9188d8274f2b648e7ff77819a42f4f505b5b8dc9
Werner Strydom <werners@bloudraak.com>
2017-01-02 01:49:08 -0800

    Update Versioning

--- tools/test.cmd
+++ tools/test.cmd
@@ -1,8 +1,8 @@
 @echo off
 
-msbuild build.proj /t:Version /p:MajorVersion=1 /p:MinorVersion=2 /p:BuildNumber=3 /p:BranchName=master /verbosity:m /nologo
-msbuild build.proj /t:Version /p:MajorVersion=1 /p:MinorVersion=2 /p:BuildNumber=3 /p:BranchName=develop /verbosity:m /nologo 
-msbuild build.proj /t:Version /p:MajorVersion=1 /p:MinorVersion=2 /p:BuildNumber=3 /p:BranchName=release/1.2 /verbosity:m /nologo
-msbuild build.proj /t:Version /p:MajorVersion=5 /p:MinorVersion=6 /p:BuildNumber=7 /p:BranchName=release/4.5.6 /verbosity:m /nologo
-msbuild build.proj /t:Version /p:MajorVersion=1 /p:MinorVersion=2 /p:BuildNumber=3 /p:BranchName=feature/PHAK-1 /verbosity:m /nologo
-msbuild build.proj /t:Version /p:MajorVersion=1 /p:MinorVersion=2 /p:BuildNumber=3 /p:BranchName=fake /verbosity:m /nologo
+msbuild build.proj /t:UpdateAssemblyVersions /p:MajorVersion=1 /p:MinorVersion=2 /p:BuildNumber=3 /p:BranchName=master /verbosity:m /nologo
+msbuild build.proj /t:UpdateAssemblyVersions /p:MajorVersion=1 /p:MinorVersion=2 /p:BuildNumber=3 /p:BranchName=develop /verbosity:m /nologo 
+msbuild build.proj /t:UpdateAssemblyVersions /p:MajorVersion=1 /p:MinorVersion=2 /p:BuildNumber=3 /p:BranchName=release/1.2 /verbosity:m /nologo
+msbuild build.proj /t:UpdateAssemblyVersions /p:MajorVersion=5 /p:MinorVersion=6 /p:BuildNumber=7 /p:BranchName=release/4.5.6 /verbosity:m /nologo
+msbuild build.proj /t:UpdateAssemblyVersions /p:MajorVersion=1 /p:MinorVersion=2 /p:BuildNumber=3 /p:BranchName=feature/PHAK-1 /verbosity:m /nologo
+msbuild build.proj /t:UpdateAssemblyVersions /p:MajorVersion=1 /p:MinorVersion=2 /p:BuildNumber=3 /p:BranchName=fake /verbosity:m /nologo
